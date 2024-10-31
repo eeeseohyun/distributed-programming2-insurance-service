2117fa3eaae5fa653de607cd2a047f31be838dbf
choieuntaek <choiet0225@gmail.com>
2024-10-31 21:59:49 +0900

    feat: 계약체결 카테고리-계약을 체결한다. 재심사 요청한다. 기능 구현

--- src/main/java/com/example/insuranceservice/domain/contract/dto/ContractDto.java
+++ src/main/java/com/example/insuranceservice/domain/contract/dto/ContractDto.java
@@ -1,11 +1,16 @@
 package com.example.insuranceservice.domain.contract.dto;
 
+import com.example.insuranceservice.domain.contract.entity.Contract;
 import com.example.insuranceservice.domain.paymentInfo.entity.PaymentInfo;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ContractDto {
     private Integer id;
     private String concludedDate;
@@ -23,4 +28,23 @@ public class ContractDto {
     private String resurrectionReason;
     private Integer underwritingEID;
     private List<PaymentInfo> paymentInfoList;
+
+    public ContractDto(Contract contract) {
+        this.id = contract.getId();
+        this.concludedDate = contract.getConcludedDate();
+        this.concludedEID = contract.getConcludedEID();
+        this.contractStatus = contract.getContractStatus();
+        this.createdDate = contract.getCreatedDate();
+        this.evaluation = contract.getEvaluation();
+        this.expirationDate = contract.getExpirationDate();
+        this.isConcluded = contract.getIsConcluded();
+        this.isPassUW = contract.getIsPassUW();
+        this.monthlyPremium = contract.getMonthlyPremium();
+        this.nonPaymentPeriod = contract.getNonPaymentPeriod();
+        this.renewalStatus = contract.getRenewalStatus();
+        this.resurrectionDate = contract.getResurrectionDate();
+        this.resurrectionReason = contract.getResurrectionReason();
+        this.underwritingEID = contract.getUnderwritingEID();
+        this.paymentInfoList = contract.getPaymentInfoList();
+    }
 }
--- src/main/java/com/example/insuranceservice/domain/contract/service/ContractService.java
+++ src/main/java/com/example/insuranceservice/domain/contract/service/ContractService.java
@@ -1,7 +1,13 @@
 package com.example.insuranceservice.domain.contract.service;
 
+import com.example.insuranceservice.domain.contract.dto.ContractDto;
+import com.example.insuranceservice.domain.contract.entity.Contract;
 import com.example.insuranceservice.domain.contract.repository.ContractRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ContractService {
@@ -10,5 +16,62 @@ public class ContractService {
 
     public ContractService(ContractRepository contractRepository){
         this.contractRepository = contractRepository;;
+    }
+    public List<ContractDto> getContractsToConclude() {
+        List<Contract> permitContracts = contractRepository.findByContractStatus("ContractPermission");
+        return permitContracts.stream().map(ContractDto::new).collect(Collectors.toList());
+    }
+
+    public List<ContractDto> getRejectedContracts() {
+        List<Contract> rejectedContracts = contractRepository.findByContractStatus("ReviewReject");
+        return rejectedContracts.stream().map(ContractDto::new).collect(Collectors.toList());
+    }
+
+    public String concludeContract(Integer contractId, boolean approve) {
+        Optional<Contract> contractOptional = contractRepository.findById(contractId);
+        if (contractOptional.isPresent()) {
+            Contract contract = contractOptional.get();
+            if (approve) {
+                contract.setContractStatus("concluded");
+                contractRepository.save(contract);
+                return "계약이 성공적으로 체결되었습니다.";
+            } else {
+                return "계약 체결이 취소되었습니다.";
+            }
+        } else {
+            return "해당 계약 id를 찾을 수 없습니다.";
+        }
+    }
+
+    public String requestReUnderwriting(Integer contractId) {
+        Optional<Contract> contractOptional = contractRepository.findById(contractId);
+        if (contractOptional.isPresent()) {
+            Contract contract = contractOptional.get();
+            contract.setContractStatus("ReviewRequest");
+            contractRepository.save(contract);
+            return "[success] 재심사 요청이 완료되었습니다.";
+        } else {
+            return "해당 계약 id를 찾을 수 없습니다.";
+        }
+    }
+
+    public String createContract(ContractDto contractDto) {
+        Contract contract = new Contract();
+        contract.setConcludedDate(contractDto.getConcludedDate());
+        contract.setConcludedEID(contractDto.getConcludedEID());
+        contract.setContractStatus(contractDto.getContractStatus());
+        contract.setCreatedDate(contractDto.getCreatedDate());
+        contract.setEvaluation(contractDto.getEvaluation());
+        contract.setExpirationDate(contractDto.getExpirationDate());
+        contract.setIsConcluded(contractDto.getIsConcluded());
+        contract.setIsPassUW(contractDto.getIsPassUW());
+        contract.setMonthlyPremium(contractDto.getMonthlyPremium());
+        contract.setNonPaymentPeriod(contractDto.getNonPaymentPeriod());
+        contract.setRenewalStatus(contractDto.getRenewalStatus());
+        contract.setResurrectionDate(contractDto.getResurrectionDate());
+        contract.setResurrectionReason(contractDto.getResurrectionReason());
+        contract.setUnderwritingEID(contractDto.getUnderwritingEID());
+        contractRepository.save(contract);
+        return "계약이 성공적으로 생성되었습니다.";
     }
 }
