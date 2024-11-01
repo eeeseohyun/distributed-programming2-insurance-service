8677e44e4a66f8cedc7ec842d3a9522f8bff159f
JunnKyuu <junnkyuu22@gmail.com>
2024-11-01 18:14:19 +0900

    feat: 사고접수 카테고리 - 사고접수 신청, 사고접수 조회, 사고접수 수정, 사고접수 삭제

--- src/main/java/com/example/insuranceservice/domain/accident/repository/AccidentRepository.java
+++ src/main/java/com/example/insuranceservice/domain/accident/repository/AccidentRepository.java
@@ -2,6 +2,11 @@ package com.example.insuranceservice.domain.accident.repository;
 
 import com.example.insuranceservice.domain.accident.entity.Accident;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface AccidentRepository extends JpaRepository<Accident, Integer> {
+    List<Accident> findByCustomerCustomerID(int customerID);
 }
