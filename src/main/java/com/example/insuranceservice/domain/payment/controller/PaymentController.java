d9f973614541a9bf4852ea188d542bec1b0fd3c1
yeilkk <yeil99@naver.com>
2024-11-02 04:32:53 +0900

    feat: 보험료 납부 리스트 조회

--- src/main/java/com/example/insuranceservice/domain/payment/controller/PaymentController.java
+++ src/main/java/com/example/insuranceservice/domain/payment/controller/PaymentController.java
@@ -1,8 +1,13 @@
 package com.example.insuranceservice.domain.payment.controller;
 
+import com.example.insuranceservice.domain.payment.dto.PaymentDto;
 import com.example.insuranceservice.domain.payment.service.PaymentService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/payments")
@@ -12,6 +17,12 @@ public class PaymentController {
 
     public PaymentController(PaymentService paymentService){
         this.paymentService = paymentService;
+    }
+    //// 보험료 납부 카테고리
+    // 보험료 납부 리스트 조회
+    @GetMapping("/list/{customerId}")
+    public List<PaymentDto> showPaymentList(@PathVariable Integer customerId){
+        return paymentService.showPaymentList(customerId);
     }
 
 }
--- src/main/java/com/example/insuranceservice/domain/payment/service/PaymentService.java
+++ src/main/java/com/example/insuranceservice/domain/payment/service/PaymentService.java
@@ -1,8 +1,14 @@
 package com.example.insuranceservice.domain.payment.service;
 
+import com.example.insuranceservice.domain.customer.entity.Customer;
 import com.example.insuranceservice.domain.customer.service.CustomerService;
+import com.example.insuranceservice.domain.payment.dto.PaymentDto;
+import com.example.insuranceservice.domain.payment.entity.Payment;
 import com.example.insuranceservice.domain.payment.repository.PaymentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PaymentService {
@@ -15,4 +21,23 @@ public class PaymentService {
         this.customerService = customerService;
     }
 
+    //// 보험료 납부 카테고리
+    // 보험료 납부 리스트 조회
+    public List<PaymentDto> showPaymentList(Integer customerId) {
+        Customer customer = customerService.findCustomerById(customerId);
+        List<Payment> paymentList = paymentRepository.findByCustomerAndStatusOfPayment(customer, false);
+        List<PaymentDto> paymentDtoList = new ArrayList<>();
+        for(Payment payment: paymentList){
+            PaymentDto paymentDto = new PaymentDto();
+            paymentDto.setId(payment.getId());
+            paymentDto.setContractId(payment.getContract().getId());
+            paymentDto.setCustomerId(payment.getCustomer().getCustomerID());
+            paymentDto.setAmount(payment.getAmount());
+            paymentDto.setDueDateOfPayment(payment.getDueDateOfPayment());
+            paymentDto.setStatusOfPayment(payment.isStatusOfPayment());
+
+            paymentDtoList.add(paymentDto);
+        }
+        return paymentDtoList;
+    }
 }
