08dd1b209a4c83a065ea009d6731497d53301b13
JunnKyuu <junnkyuu22@gmail.com>
2024-11-02 00:48:52 +0900

    fix: AccidentController 주석 추가

--- src/main/java/com/example/insuranceservice/domain/accident/controller/AccidentController.java
+++ src/main/java/com/example/insuranceservice/domain/accident/controller/AccidentController.java
@@ -14,26 +14,32 @@ import java.util.List;
 public class AccidentController {
     private final AccidentService accidentService;
 
+    //// 사고접수 카테고리
+    // 사고접수 조회 - 모든 사고 조회
     @GetMapping
     public ResponseEntity<List<AccidentDTO>> getAllAccidents() {
         return ResponseEntity.ok(accidentService.getAllAccidents());
     }
 
+    // 사고접수 조회 - accidentID 이용
     @GetMapping("/{accidentID}")
     public ResponseEntity<AccidentDTO> getAccidentById(@PathVariable int accidentID) {
         return ResponseEntity.ok(accidentService.getAccidentById(accidentID));
     }
 
+    // 사고접수 조회 - customerID 이용
     @GetMapping("/customer/{customerID}")
     public ResponseEntity<List<AccidentDTO>> getAccidentsByCustomerId(@PathVariable int customerID) {
         return ResponseEntity.ok(accidentService.getAccidentsByCustomerId(customerID));
     }
 
+    // 사고접수 신청
     @PostMapping
     public ResponseEntity<AccidentDTO> createAccident(@RequestBody AccidentDTO accidentDTO) {
         return new ResponseEntity<>(accidentService.createAccident(accidentDTO), HttpStatus.CREATED);
     }
 
+    // 사고접수 수정
     @PutMapping("/{accidentID}")
     public ResponseEntity<AccidentDTO> updateAccident(
             @PathVariable int accidentID,
@@ -41,9 +47,11 @@ public class AccidentController {
         return ResponseEntity.ok(accidentService.updateAccident(accidentID, accidentDTO));
     }
 
+    // 사고접수 삭제 - 직원만 가능
     @DeleteMapping("/{accidentID}")
     public ResponseEntity<Void> deleteAccident(@PathVariable int accidentID) {
         accidentService.deleteAccident(accidentID);
         return ResponseEntity.noContent().build();
     }
+    ////
 }
