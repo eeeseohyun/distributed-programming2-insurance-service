73f5b1c2a5c87ef9793a4737b712b97678cc1784
eeeseohyun <eeeseohyun@gmail.com>
2024-11-25 13:39:45 +0900

    권한 추가

--- src/main/java/com/example/insuranceservice/domain/user/jwt/SecurityConfig.java
+++ src/main/java/com/example/insuranceservice/domain/user/jwt/SecurityConfig.java
@@ -41,6 +41,7 @@ public class SecurityConfig {
                         .requestMatchers("api/compensations/createCompensation","api/compensations/showCompensationList","api/compensations/requestInsuranceAmount").hasRole(Constant.Customer)
                         .requestMatchers("api/compensations/showAllCompensationList","api/compensations/updateCompensation","api/compensations/deleteCompensation","api/compensations/investigateLoss","api/compensations/calculateInsuranceAmount","api/compensations/giveInsuranceAmount").hasRole(Constant.CompensationProcessing)
                         .requestMatchers("api/counsels/requested/list","api/counsels/confirm","api/counsels/confirmed/list","api/counsels/consulted/list","api/consulted/list","api/update","api/suggest").hasRole(Constant.Sales)
+                        .requestMatchers("api/customers/createCustomer").hasRole(Constant.CutomerInfomationManage)
                         .anyRequest().hasRole(Constant.Employee))
                 // JWT 인증을 위하여 직접 구현한 필터를 UsernamePasswordAuthenticationFilter 전에 실행
                 .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider),
